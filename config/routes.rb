66b3025a62d31d467447006bbcfacabb86e9f54f
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2020-06-27 13:29:22 +0200

    new pages about and contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'about', to:'pages#about', as: :about
+
+  get 'contact', to:'pages#contact', as: :contact
+
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
