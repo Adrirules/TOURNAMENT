9a038e906aed6db040821b6619db70644c11229f
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2020-06-28 19:06:38 +0200

    creating routes for tournaments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   get 'about', to:'pages#about', as: :about
-
   get 'contact', to:'pages#contact', as: :contact
+
+  resources :tournaments
 
 
   devise_for :users
