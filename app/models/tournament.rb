d5ee70332593cdac9993df44402346a58492062d
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2020-06-28 17:41:16 +0200

    add validation on tournament

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,2 +1,12 @@
 class Tournament < ApplicationRecord
+
+  validates :name, presence: true
+  validates :start_tournament, presence: true
+  validates :end_tournament, presence: true
+  validates :photo, presence: true
+  validates :activity, presence: true
+
+
+
+
 end
